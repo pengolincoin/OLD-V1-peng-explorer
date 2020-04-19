1459fe6bac074af6e16aa4cfca47b900473e50d9
PengolinCoin <62501983+pengolincoin@users.noreply.github.com>
2020-04-19 08:06:39 +0200

    Update index.php

--- q/reward/index.php
+++ q/reward/index.php
@@ -1,4 +1,5 @@
 <?php
+/*
 require '../util.php';
 $config = (require '../../config.php');
 
@@ -8,3 +9,15 @@ $rewardRaw = $blockHeader['result']['block_header']['reward'];
 $reward = number_format($rewardRaw / 100, 2, ".", "");
 
 print_r($reward);
+*/
+
+require dirname(__FILE__) . '/../util.php';
+$config = (require dirname(__FILE__) . '/../../config.php');
+
+$blockHeader = url_get_contents_post($config['api'], 'getlastblockheader', '" "');
+$rewardRaw = $blockHeader['result']['block_header']['reward'];
+$reward = number_format($rewardRaw / 100000000, 8, ".", "");
+
+print_r($reward);
+
+?>
