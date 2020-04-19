70ac725716896b2f1ea7d096a1bd51448e6042eb
PengolinCoin <62501983+pengolincoin@users.noreply.github.com>
2020-04-19 08:06:02 +0200

    Update index.php

--- q/height/index.php
+++ q/height/index.php
@@ -1,7 +1,18 @@
 <?php
+/*
 require '../util.php';
 $config = (require '../../config.php');
 
 $info = fetch_getinfo($config['api']);
 
 print_r($info['last_known_block_index']);
+*/
+
+require dirname(__FILE__) . '/../util.php';
+$config = (require dirname(__FILE__) . '/../../config.php');
+
+$info = fetch_getinfo($config['api']);
+
+print_r($info['last_known_block_index']);
+
+?>
